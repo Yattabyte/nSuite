00ef5d23d5082f160ecfd10f7001d62ff69cc5e8
Troy Lowry <tklowry93@gmail.com>
2020-01-11 22:17:10 -0600

    adding back more unit tests

--- tests/UnitTests/main.cpp
+++ tests/UnitTests/main.cpp
@@ -11,11 +11,11 @@ using yatta::Directory;
 
 //// Forward Declarations
 static bool Buffer_ConstructionTest();
-//static bool Buffer_AssignmentTest();
-//static bool Buffer_MethodTest();
-//static bool Buffer_IOTest();
-//static bool Buffer_CompressionTest();
-//static bool Buffer_DiffTest();
+static bool Buffer_AssignmentTest();
+static bool Buffer_MethodTest();
+static bool Buffer_IOTest();
+static bool Buffer_CompressionTest();
+static bool Buffer_DiffTest();
 //static bool BufferView_ConstructionTest();
 //static bool BufferView_AssignmentTest();
 //static bool BufferView_MethodTest();
@@ -47,12 +47,12 @@ static bool RunTests(const FirstFunc& firstFunc, const RestFuncs&... restFuncs)
 int main()
 {
     return RunTests(
-        Buffer_ConstructionTest/*,
+        Buffer_ConstructionTest,
         Buffer_AssignmentTest,
         Buffer_MethodTest,
         Buffer_IOTest,
         Buffer_CompressionTest,
-        Buffer_DiffTest,
+        Buffer_DiffTest/*,
 
         BufferView_ConstructionTest,
         BufferView_AssignmentTest,
@@ -102,185 +102,185 @@ static bool Buffer_ConstructionTest()
     return false; // Failure
 }
 
-//static bool Buffer_AssignmentTest()
-//{
-//    try {
-//        Buffer bufferA(1234ULL);
-//        Buffer bufferB(123456789ULL);
-//        bufferA[0] = static_cast<std::byte>(255U);
-//        bufferB[0] = static_cast<std::byte>(126U);
-//
-//        // Ensure buffers are equal
-//        // To Do: Add equality operator
-//        bufferA = bufferB;
-//        if (bufferA[0] == bufferB[0]) {
-//            Buffer bufferC(456);
-//            bufferC[0] = static_cast<std::byte>(64U);
-//            bufferA = std::move(bufferC);
-//            // Ensure bufferC is fully moved over to bufferA
-//            if (bufferA[0] == static_cast<std::byte>(64U)) {
-//                std::cout << "Buffer Assignment Test - Success\n";
-//                return true; // Success
-//            }
-//        }
-//    }
-//    catch (const std::exception & e) {
-//        std::cout << e.what() << "\n";
-//    }
-//
-//    std::cout << "Buffer Assignment Test - Failure\n";
-//    return false; // Failure
-//}
-//
-//static bool Buffer_MethodTest()
-//{
-//    try {
-//        Buffer buffer;
-//        // Ensure the buffer is empty
-//        if (buffer.empty()) {
-//            buffer.resize(1234ULL);
-//            // Ensure we can resize the buffer with data
-//            if (buffer.hasData()) {
-//                // Ensure the size is 1234
-//                if (buffer.size() == 1234ULL) {
-//                    // Ensure the capacity is doubled
-//                    if (buffer.capacity() == 2468ULL) {
-//                        // Ensure we can hash the buffer
-//                        if (const auto hash = buffer.hash(); hash != 0ULL && hash != 1234567890ULL) {
-//                            // Ensure we can return a char array
-//                            if (const auto cArray = buffer.charArray(); cArray != nullptr) {
-//                                // Ensure we can return a byte array
-//                                if (const auto bytes = buffer.bytes(); bytes != nullptr) {
-//                                    // Ensure both char array and byte array are the same underlying pointer
-//                                    if (static_cast<const void*>(cArray) == static_cast<const void*>(bytes)) {
-//                                        std::cout << "Buffer Method Test - Success\n";
-//                                        return true; // Success
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//    catch (const std::exception & e) {
-//        std::cout << e.what() << "\n";
-//    }
-//
-//    std::cout << "Buffer Method Test - Failure\n";
-//    return false; // Failure
-//}
-//
-//static bool Buffer_IOTest()
-//{
-//    try {
-//        // Ensure object IO is correct
-//        Buffer buffer(sizeof(int));
-//        constexpr int in_int(64);
-//        buffer.in_type(in_int);
-//        int out_int(0);
-//        buffer.out_type(out_int);
-//        if (in_int == out_int) {
-//            // Ensure raw pointer IO is correct
-//            const char word[28] = ("This is a sample sentence.\0");
-//            constexpr auto sentenceSize = sizeof(char) * 28;
-//            buffer.resize(sentenceSize + sizeof(int));
-//            buffer.in_raw(&word, sentenceSize, sizeof(int));
-//
-//            struct TestStructure {
-//                int a;
-//                char b[28];
-//            } combinedOutput{};
-//            buffer.out_raw(&combinedOutput, sizeof(TestStructure));
-//
-//            if (combinedOutput.a == in_int && std::string(combinedOutput.b) == word) {
-//                std::cout << "Buffer IO Test - Success\n";
-//                return true; // Success
-//            }
-//        }
-//    }
-//    catch (const std::exception & e) {
-//        std::cout << e.what() << "\n";
-//    }
-//
-//    std::cout << "Buffer IO Test - Failure\n";
-//    return false; // Failure
-//}
-//
-//static bool Buffer_CompressionTest()
-//{
-//    try {
-//        // The structure we'll compress and decompress
-//        struct TestStructure {
-//            int a = 0;
-//            float b = 0.F;
-//            char c[32] = { '\0' };
-//        };
-//
-//        // Create a buffer and load it with test data
-//        Buffer buffer(sizeof(TestStructure));
-//        constexpr TestStructure testData{ 1234, 567.890F,"QWEQWEQWEQWEQWEQWEQWEQWEQWEQWE\0" };
-//        buffer.in_type(testData);
-//
-//        // Attempt to compress the buffer
-//        const auto compressedBuffer = buffer.compress();
-//        // Attempt to decompress the compressed buffer
-//        const auto decompressedBuffer = compressedBuffer.decompress();
-//        // Dump buffer data back into test structure
-//        TestStructure decompressedData;
-//        decompressedBuffer.out_type(decompressedData);
-//
-//        // Ensure data matches
-//        if (testData.a == decompressedData.a && testData.b == decompressedData.b && std::strcmp(testData.c, decompressedData.c) == 0) {
-//            std::cout << "Buffer Compression/Decompression Test - Success\n";
-//            return true; // Success
-//        }
-//
-//    }
-//    catch (const std::exception & e) {
-//        std::cout << e.what() << "\n";
-//    }
-//    std::cout << "Buffer Compression/Decompression Test - Failure\n";
-//    return false; // Failure
-//}
-//
-//static bool Buffer_DiffTest()
-//{
-//    try {
-//        struct Foo {
-//            int a = 0;
-//            float b = 0.0F;
-//            char c[28] = { '\0' };
-//        } dataA{ 1234, 567.890F, "This is a sample sentence.\0" };
-//        struct Bar {
-//            char a[32] = { '\0' };
-//            float b = 0.0F;
-//            int c = 0;
-//        } dataB{ "Not even a sample sentence.\0", 890.567F, 4321 };
-//        Buffer bufferA(sizeof(Foo));
-//        Buffer bufferB(sizeof(Bar));
-//        bufferA.in_type(dataA);
-//        bufferB.in_type(dataB);
-//
-//        // Ensure we've generated an instruction set
-//        const auto diffBuffer = bufferA.diff(bufferB, 8);
-//        // Check that we've actually converted from A to B
-//        const auto patchedBuffer = bufferA.patch(diffBuffer);
-//        Bar dataC;
-//        patchedBuffer.out_type(dataC);
-//        if (std::strcmp(dataB.a, dataC.a) == 0 && dataB.b == dataC.b && dataB.c == dataC.c) {
-//            std::cout << "Buffer Diff/Patch Test - Success\n";
-//            return true; // Success
-//        }         
-//    }
-//    catch (const std::exception & e) {
-//        std::cout << e.what() << "\n";
-//    }
-//    std::cout << "Buffer Diff/Patch Test - Failure\n";
-//    return false; // Failure
-//}
-//
+static bool Buffer_AssignmentTest()
+{
+    try {
+        Buffer bufferA(1234ULL);
+        Buffer bufferB(123456789ULL);
+        bufferA[0] = static_cast<std::byte>(255U);
+        bufferB[0] = static_cast<std::byte>(126U);
+
+        // Ensure buffers are equal
+        // To Do: Add equality operator
+        bufferA = bufferB;
+        if (bufferA[0] == bufferB[0]) {
+            Buffer bufferC(456);
+            bufferC[0] = static_cast<std::byte>(64U);
+            bufferA = std::move(bufferC);
+            // Ensure bufferC is fully moved over to bufferA
+            if (bufferA[0] == static_cast<std::byte>(64U)) {
+                std::cout << "Buffer Assignment Test - Success\n";
+                return true; // Success
+            }
+        }
+    }
+    catch (const std::exception & e) {
+        std::cout << e.what() << "\n";
+    }
+
+    std::cout << "Buffer Assignment Test - Failure\n";
+    return false; // Failure
+}
+
+static bool Buffer_MethodTest()
+{
+    try {
+        Buffer buffer;
+        // Ensure the buffer is empty
+        if (buffer.empty()) {
+            buffer.resize(1234ULL);
+            // Ensure we can resize the buffer with data
+            if (buffer.hasData()) {
+                // Ensure the size is 1234
+                if (buffer.size() == 1234ULL) {
+                    // Ensure the capacity is doubled
+                    if (buffer.capacity() == 2468ULL) {
+                        // Ensure we can hash the buffer
+                        if (const auto hash = buffer.hash(); hash != 0ULL && hash != 1234567890ULL) {
+                            // Ensure we can return a char array
+                            if (const auto cArray = buffer.charArray(); cArray != nullptr) {
+                                // Ensure we can return a byte array
+                                if (const auto bytes = buffer.bytes(); bytes != nullptr) {
+                                    // Ensure both char array and byte array are the same underlying pointer
+                                    if (static_cast<const void*>(cArray) == static_cast<const void*>(bytes)) {
+                                        std::cout << "Buffer Method Test - Success\n";
+                                        return true; // Success
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    catch (const std::exception & e) {
+        std::cout << e.what() << "\n";
+    }
+
+    std::cout << "Buffer Method Test - Failure\n";
+    return false; // Failure
+}
+
+static bool Buffer_IOTest()
+{
+    try {
+        // Ensure object IO is correct
+        Buffer buffer(sizeof(int));
+        constexpr int in_int(64);
+        buffer.in_type(in_int);
+        int out_int(0);
+        buffer.out_type(out_int);
+        if (in_int == out_int) {
+            // Ensure raw pointer IO is correct
+            const char word[28] = ("This is a sample sentence.\0");
+            constexpr auto sentenceSize = sizeof(char) * 28;
+            buffer.resize(sentenceSize + sizeof(int));
+            buffer.in_raw(&word, sentenceSize, sizeof(int));
+
+            struct TestStructure {
+                int a;
+                char b[28];
+            } combinedOutput{};
+            buffer.out_raw(&combinedOutput, sizeof(TestStructure));
+
+            if (combinedOutput.a == in_int && std::string(combinedOutput.b) == word) {
+                std::cout << "Buffer IO Test - Success\n";
+                return true; // Success
+            }
+        }
+    }
+    catch (const std::exception & e) {
+        std::cout << e.what() << "\n";
+    }
+
+    std::cout << "Buffer IO Test - Failure\n";
+    return false; // Failure
+}
+
+static bool Buffer_CompressionTest()
+{
+    try {
+        // The structure we'll compress and decompress
+        struct TestStructure {
+            int a = 0;
+            float b = 0.F;
+            char c[32] = { '\0' };
+        };
+
+        // Create a buffer and load it with test data
+        Buffer buffer(sizeof(TestStructure));
+        constexpr TestStructure testData{ 1234, 567.890F,"QWEQWEQWEQWEQWEQWEQWEQWEQWEQWE\0" };
+        buffer.in_type(testData);
+
+        // Attempt to compress the buffer
+        const auto compressedBuffer = buffer.compress();
+        // Attempt to decompress the compressed buffer
+        const auto decompressedBuffer = compressedBuffer.decompress();
+        // Dump buffer data back into test structure
+        TestStructure decompressedData;
+        decompressedBuffer.out_type(decompressedData);
+
+        // Ensure data matches
+        if (testData.a == decompressedData.a && testData.b == decompressedData.b && std::strcmp(testData.c, decompressedData.c) == 0) {
+            std::cout << "Buffer Compression/Decompression Test - Success\n";
+            return true; // Success
+        }
+
+    }
+    catch (const std::exception & e) {
+        std::cout << e.what() << "\n";
+    }
+    std::cout << "Buffer Compression/Decompression Test - Failure\n";
+    return false; // Failure
+}
+
+static bool Buffer_DiffTest()
+{
+    try {
+        struct Foo {
+            int a = 0;
+            float b = 0.0F;
+            char c[28] = { '\0' };
+        } dataA{ 1234, 567.890F, "This is a sample sentence.\0" };
+        struct Bar {
+            char a[32] = { '\0' };
+            float b = 0.0F;
+            int c = 0;
+        } dataB{ "Not even a sample sentence.\0", 890.567F, 4321 };
+        Buffer bufferA(sizeof(Foo));
+        Buffer bufferB(sizeof(Bar));
+        bufferA.in_type(dataA);
+        bufferB.in_type(dataB);
+
+        // Ensure we've generated an instruction set
+        const auto diffBuffer = bufferA.diff(bufferB, 8);
+        // Check that we've actually converted from A to B
+        const auto patchedBuffer = bufferA.patch(diffBuffer);
+        Bar dataC;
+        patchedBuffer.out_type(dataC);
+        if (std::strcmp(dataB.a, dataC.a) == 0 && dataB.b == dataC.b && dataB.c == dataC.c) {
+            std::cout << "Buffer Diff/Patch Test - Success\n";
+            return true; // Success
+        }         
+    }
+    catch (const std::exception & e) {
+        std::cout << e.what() << "\n";
+    }
+    std::cout << "Buffer Diff/Patch Test - Failure\n";
+    return false; // Failure
+}
+
 //static bool BufferView_ConstructionTest()
 //{
 //    try {
