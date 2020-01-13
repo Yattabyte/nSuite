f1b2a89f371be3de658ae0060dcb141269970adc
Troy Lowry <tklowry93@gmail.com>
2020-01-13 15:16:24 -0600

    attempt to fix filesystem issue

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -10,42 +10,41 @@ using yatta::Directory;
 
 Directory::Directory(const std::filesystem::path& path, const std::vector<std::string>& exclusions)
 {
-    //if (std::filesystem::is_directory(path))
+    if (std::filesystem::is_directory(path))
         in_folder(path, exclusions);
 }
 
-void Directory::in_folder(const std::filesystem::path&, const std::vector<std::string>&)
+void Directory::in_folder(const std::filesystem::path& path, const std::vector<std::string>& exclusions)
 {
-    std::vector<std::filesystem::directory_entry> paths;
-    //constexpr auto get_file_paths = [](const std::filesystem::path&, const std::vector<std::string>&) {
-    //    //constexpr auto check_exclusion = [](const std::filesystem::path& path, const std::vector<std::string>& exclusions) {
-    //    //    const auto extension = path.extension();
-    //    //    for (const auto& excl : exclusions) {
-    //    //        if (excl.empty())
-    //    //            continue;
-    //    //        // Compare Paths && Extensions
-    //    //        if (path == excl || extension == excl) {
-    //    //            // Don't use path
-    //    //            return false;
-    //    //        }
-    //    //    }
-    //    //    // Safe to use path
-    //    //    return true;
-    //    //};
-    //    std::vector<std::filesystem::directory_entry> paths;
-    //   // if (std::filesystem::is_directory(directory)) {}
-    //        /*for (const auto& entry : std::filesystem::recursive_directory_iterator(directory))
-    //            if (entry.is_regular_file()) {
-    //                auto path = entry.path().string();
-    //                path = path.substr(directory.string().size(), path.size() - directory.string().size());
-    //                //if (check_exclusion(path, exclusions))
-    //                    paths.emplace_back(entry);
-    //            }*/
-    //    return paths;
-    //};
-    for (const auto& entry : paths) {
+    constexpr auto get_file_paths = [](const std::filesystem::path& directory, const std::vector<std::string>& exclusions) {
+        constexpr auto check_exclusion = [](const std::filesystem::path& path, const std::vector<std::string>& exclusions) {
+            const auto extension = path.extension();
+            for (const auto& excl : exclusions) {
+                if (excl.empty())
+                    continue;
+                // Compare Paths && Extensions
+                if (path == excl || extension == excl) {
+                    // Don't use path
+                    return false;
+                }
+            }
+            // Safe to use path
+            return true;
+        };
+        std::vector<std::filesystem::directory_entry> paths;
+        if (std::filesystem::is_directory(directory))
+            for (const auto& entry : std::filesystem::recursive_directory_iterator(directory))
+                if (entry.is_regular_file()) {
+                    auto path = entry.path().string();
+                    path = path.substr(directory.string().size(), path.size() - directory.string().size());
+                    if (check_exclusion(path, exclusions))
+                        paths.emplace_back(entry);
+                }
+        return paths;
+    };
+    for (const auto& entry : get_file_paths(path, exclusions)) {
         if (entry.is_regular_file()) {
-            /*// Read the file data
+            // Read the file data
             Buffer fileBuffer(entry.file_size());
             const std::string path_string = entry.path().string();
             constexpr std::ios_base::openmode mode = std::ios_base::in | std::ios_base::binary;
@@ -56,7 +55,7 @@ void Directory::in_folder(const std::filesystem::path&, const std::vector<std::s
             fileOnDisk.read(fileBuffer.charArray(), static_cast<std::streamsize>(fileBuffer.size()));
             fileOnDisk.close();
 
-            m_files.emplace_back(VirtualFile{ (std::filesystem::relative(entry.path(), path)).string(), std::move(fileBuffer) });*/
+            m_files.emplace_back(VirtualFile{ (std::filesystem::relative(entry.path(), path)).string(), std::move(fileBuffer) });
         }
     }
 }
