baf66625248e915302c3b886b0186be0cab396bc
Troy Lowry <tklowry93@gmail.com>
2020-01-14 09:58:46 -0600

    directory test
    directory test

--- tests/UnitTests/main.cpp
+++ tests/UnitTests/main.cpp
@@ -656,6 +656,10 @@ static bool Directory_ConstructionTest()
             Directory dirA(Directory::GetRunningDirectory() + "\\old");
             for (const auto& a : std::filesystem::recursive_directory_iterator(Directory::GetRunningDirectory()))
                 std::cout << a.path() << "\n";
+            if (std::filesystem::is_directory(Directory::GetRunningDirectory() + "\\old"))
+                std::cout << "found path\n";
+            else
+                std::cout << "didn't find path\n";
             if (dirA.hasFiles()) {
                 std::cout << "Directory has " << dirA.fileCount() << " files, totalling " << dirA.fileSize() << " bytes";
     //            // Ensure move constructor works
