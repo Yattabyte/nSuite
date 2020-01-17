ae37eb5dc9570e93ad8b0a5d255c3dc79a700376
Troy Lowry <tklowry93@gmail.com>
2020-01-16 23:23:18 -0600

    add valgrind to cmake

--- buildDebug.sh
+++ buildDebug.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DCMAKE_BUILD_TYPE=Debug . || exit 1
+cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug . || exit 1
 cmake --build . --clean-first -- -j $(nproc) || exit 1
 ctest --verbose --output-on-failure -j $(nproc) || exit 1
--- codeCoverage.sh
+++ codeCoverage.sh
@@ -3,7 +3,7 @@
 if [ ${CODE_COVERAGE} = true ]; then
   lcov --version
   gcov --version
-  cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fprofile-arcs -ftest-coverage --coverage" . || exit 1
+  cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=ON -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fprofile-arcs -ftest-coverage --coverage" . || exit 1
   cmake --build . -- -j $(nproc) || exit 1
   ctest --verbose --output-on-failure -j $(nproc) || exit 1
   lcov --directory . --capture --output-file coverage.info --gcov-tool gcov-8
