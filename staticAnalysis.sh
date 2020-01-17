67f032eb6d7a9315d7c672b269c82c62281d9d17
Troy Lowry <tklowry93@gmail.com>
2020-01-17 13:12:04 -0600

    Add remaining tests back
    Add remaining tests back

--- staticAnalysis.sh
+++ staticAnalysis.sh
@@ -4,11 +4,6 @@
 echo "Starting Clang-Tidy"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_CLANG_TIDY=clang-tidy . || exit 1
 cmake --build . --clean-first -- -j $(nproc) || exit 1
-  
-# Run Valgrind
-echo "Starting Memory Sanitizer using Valgrind"
-ctest --verbose --output-on-failure -j $(nproc) -D ExperimentalMemCheck . || exit 1
-echo "$(<${TRAVIS_BUILD_DIR}/Testing/Temporary/MemoryChecker.1.log)"
 
 # Address Sanitizers
 echo "Starting Address Sanitizer"
@@ -27,6 +22,11 @@ echo "Starting Thread Sanitizer"
 cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=thread" . || exit 1 
 cmake --build . --clean-first -- -j $(nproc) || exit 1
 ctest --verbose --output-on-failure -j $(nproc) . || exit 1 
+
+# Run Valgrind
+echo "Starting Memory Sanitizer using Valgrind"
+ctest --verbose --output-on-failure -j $(nproc) -D ExperimentalMemCheck . || exit 1
+echo "$(<${TRAVIS_BUILD_DIR}/Testing/Temporary/MemoryChecker.1.log)"
   
 # Run OCLint
 echo "Starting OCLint"
