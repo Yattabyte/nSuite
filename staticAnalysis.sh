8df931e3704040a2ad007663771043abee2bf4e8
Troy Lowry <tklowry93@gmail.com>
2020-02-01 12:21:34 -0600

    1.11.19
    Trying to fix address sanitizer

--- staticAnalysis.sh
+++ staticAnalysis.sh
@@ -26,7 +26,7 @@ echo "
 **************************************************
 Starting Address Sanitizer
 **************************************************"
-cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=address" -UCMAKE_CXX_CLANG_TIDY .
+cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls" -UCMAKE_CXX_CLANG_TIDY .
 cmake --build . --clean-first -- -j $(nproc)
 ctest --output-on-failure -j $(nproc) -C Debug .
 
