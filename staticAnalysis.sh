a369b48f9adfd2b90a62019e4844ed46540174b8
Troy Lowry <tklowry93@gmail.com>
2020-02-06 14:55:03 -0600

    Travis CI test
    Travis CI test

--- staticAnalysis.sh
+++ staticAnalysis.sh
@@ -3,8 +3,26 @@
 # Address Sanitizers
 echo "
 **************************************************
-Starting Address, U.B., and Thread Sanitizers
+Starting Address Sanitizer
 **************************************************"
-cmake -DBUILD_TESTING=ON -DCODE_COVERAGE=OFF -DSTATIC_ANALYSIS=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=address,undefined,thread -fno-omit-frame-pointer -fno-optimize-sibling-calls" -UCMAKE_CXX_CLANG_TIDY .
-cmake --build . --clean-first -- -j $(nproc)
+cmake -DSTATIC_ANALYSIS=OFF -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls" -UCMAKE_CXX_CLANG_TIDY .
+cmake --build . -- -j $(nproc)
+ctest --output-on-failure -j $(nproc) -C Debug .
+
+# Undefined Behaviour Sanitizer
+echo "
+**************************************************
+Starting Undefined-Behaviour Sanitizer
+**************************************************"
+cmake -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=undefined -fno-omit-frame-pointer -fno-optimize-sibling-calls" .
+cmake --build . -- -j $(nproc)
+ctest --output-on-failure -j $(nproc) -C Debug .
+
+# Thread Sanitizer
+echo "
+**************************************************
+Starting Thread Sanitizer
+**************************************************"
+cmake -DCMAKE_CXX_FLAGS="-g -O0 -fsanitize=thread -fno-omit-frame-pointer -fno-optimize-sibling-calls" .
+cmake --build . -- -j $(nproc)
 ctest --output-on-failure -j $(nproc) -C Debug .
